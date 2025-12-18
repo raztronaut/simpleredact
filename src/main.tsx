5f56af7f972a4b66d622a0c2e7a8d576364cf369
Razi Syed <syed.raziulhaque@gmail.com>
2025-12-18 14:29:37 -0500

    .

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,6 @@ import './index.css'
 import App from './App.tsx'
 
 
-// Inject Umami Analytics
-const umamiId = import.meta.env.VITE_UMAMI_WEBSITE_ID
-if (umamiId) {
-  const script = document.createElement('script')
-  script.defer = true
-  script.src = '/stats/script.js'
-  script.setAttribute('data-website-id', umamiId)
-  document.head.appendChild(script)
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
